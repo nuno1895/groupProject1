9b4285e60507b4ecfa9865b9cc4548c040944858
dw37928n <douglas.wallner@gmail.com>
2017-01-07 13:46:12 -0500

    15

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,3 @@
-// mlbTeams = ["Orioles","Diamondbacks", "RedSox", "Braves", "WhiteSox",   "Cubs", "Indians", "Tigers", "Rockies", "Astros", "Dodgers", "Royals",  "Marlins", "Angels", "Brewers", "Twins", "Mets", "Yankees", "Phillies", "Athletics", "Pirates", "Mariners", "Padres", "Rays", "Giants", "Rangers", "Cardinals", "BlueJays", "Nationals"]
-
-// for (var i = 0; i < mlbTeams.length; i++) {
-//   var img = $("<img>");
-//         img.attr("src", "assets/images/" + mlbTeams[i]+ ".jpg");
-//         img.attr("alt", mlbTeams[i]);
-//   $('#logosHere').append(img)
-// }
  var selectedTeam, selectedState, selectedCity, queryTeam;
   $(".team").on("click", function(){
  
@@ -33,19 +25,21 @@ var howLong = moment("20170402", "YYYYMMDD").fromNow();
 var p = $("<p>");
         p.text(howLong);
   $('#openingDay').append(p)
+
+
 $("#buttonSearch").on("click", function(){
  
     $("#resultsTarget").empty(); // Clear previous search result
 
     var stateId, startDate, endDate; //  initialize variables for search
-    // 1) get content from the form
+   
   
     startDate = $("#startDate").val();
     endDate = $("#endDate").val();
     startDate += "T00:00:00Z"
     endDate += "T00:00:00Z"
 
-    // 2) build search query
+
     var API_key_value = "&apikey=OtZP0uNORyGGudirFRnAFeu6VJ6ix8Kq";
     
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + selectedTeam + API_key_value;
@@ -58,10 +52,10 @@ $("#buttonSearch").on("click", function(){
     })
     .done(function(response) {
 
-      var eventsArray = response._embedded.events; // gets an array of articles
+var eventsArray = response._embedded.events; // gets an array of articles
         console.log(eventsArray);
 
-      // 1) create a target div to append each event to!
+// 1) create a target div to append each event to!
         var resultsContainer = $("<div>");
       // 2) loop through the array of articles & get key data
         eventsArray.forEach(function(event){
@@ -78,7 +72,8 @@ $("#buttonSearch").on("click", function(){
           });
 
     }); 
-    var weatherURL = "https://api.wunderground.com/api/517830656e79f22a/hourly10day/q/"
+// 10 day hourly forecast AJAX
+var weatherURL = "https://api.wunderground.com/api/517830656e79f22a/hourly10day/q/"
     $.ajax({
       url: weatherURL + selectedState + "/" + selectedCity + ".json",
       method: "GET",
@@ -88,14 +83,14 @@ $("#buttonSearch").on("click", function(){
       console.log(weatherData)
      });
 
-  var searchFor = selectedCity + " " + selectedTeam;
-    var numRecs = 6;
+var searchFor = selectedCity + " " + selectedTeam;
+var numRecs = 6;
 
-    var  appKey = "bd02f499a8474a05bd68ce25460bbc9f"
+var  appKey = "bd02f499a8474a05bd68ce25460bbc9f"
   
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";  
-      url += "?api-key=" + appKey ;
-      url += "&q=" +  queryTeam ;
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";  
+    url += "?api-key=" + appKey ;
+    url += "&q=" +  queryTeam ;
    
       $.ajax({
         url: url,
@@ -106,12 +101,12 @@ $("#buttonSearch").on("click", function(){
           console.log(results);
         })
 
-    var espnURL = "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=3ebf094d9f3c4221a56ceb144325c77f"
+var espnURL = "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=3ebf094d9f3c4221a56ceb144325c77f"
 
-      $.ajax({
+   $.ajax({
         url: espnURL,
         method: 'GET',
-      })
+    })
 
       .done(function(headlines) { 
         
@@ -122,5 +117,4 @@ $("#buttonSearch").on("click", function(){
 
 
 
-  
 
